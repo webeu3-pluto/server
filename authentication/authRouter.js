427bf123f4ff4ef684a964c2fc034aac26ee947f
Josemaria <josemarianriagu25@gmail.com>
2019-07-31 04:02:09 +0800

    refactor authRouter

--- authentication/authRouter.js
+++ authentication/authRouter.js
@@ -1,24 +1,23 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
-const { validateLogin } = require('../helpers/middlware');
 const Users = require('../helpers/dbModel');
 
 router.post('/register', async (req, res) => {
    const { password } = req.body;
+   const token = req.token;
    const hashed = bcrypt.hashSync(password, 14);
    req.body.password = hashed;
    try {
       const { id, firstName, lastName } = await Users.insertUser(req.body);
-      res.status(201).json({ message: `Hooray! Welcome Aboard, ${firstName}!!`, id, firstName, lastName })
+      res.status(201).json({ message: `Hooray! Welcome Aboard, ${firstName}!!`, id, firstName, lastName, token })
    }
    catch (error) {
-      console.log(error)
       res.status(500).json('Oops! We missed that. Hang on, let\'s fix it together');
    };
 });
 
-router.post('/login', validateLogin, (req, res) => {
+router.post('/login', (req, res) => {
    try {
       const token = req.token;
       res.status(200).json({ message: `Welcome ${req.user.firstName}!`, token });
