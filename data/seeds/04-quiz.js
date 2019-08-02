3e1b56b451b4f1b0e9172cd56e0e8c0a053c4f30
Josemaria <josemarianriagu25@gmail.com>
2019-08-02 18:07:54 +0800

    update seed files

--- data/seeds/04-quiz.js
+++ data/seeds/04-quiz.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid');
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
@@ -5,20 +6,20 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('quiz').insert([
-        { cat_id: 1, subcat_id: 1, teamlead_id: 1, published: true },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 1, published: false },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 2, published: true },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 2, published: false },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 3, published: true },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 3, published: false },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 4, published: true },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 4, published: false },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 14, published: true },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 14, published: false },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 15, published: true },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 15, published: false },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 19, published: true },
-        { cat_id: 1, subcat_id: 1, teamlead_id: 19, published: false }
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 1, published: true },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 1, published: false },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 2, published: true },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 2, published: false },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 3, published: true },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 3, published: false },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 4, published: true },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 4, published: false },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 14, published: true },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 14, published: false },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 15, published: true },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 15, published: false },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 19, published: true },
+        { uuid: uuid(), cat_id: 1, subcat_id: 1, teamlead_id: 19, published: false }
       ]);
     });
 };
