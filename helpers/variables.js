b0d378dd419a03eec19b41cc59ae7f1635a8a326
Josemaria <josemarianriagu25@gmail.com>
2019-08-14 18:32:33 +0800

    store variables in a central file

---
+++ helpers/variables.js
@@ -0,0 +1,22 @@
+module.exports = {
+   welcomeMessage: `<h2>Welcome to Pluto API!!</h2>`,
+   errorMessage: 'Oops! We missed that.Hang on, let\'s fix it together',
+   regWelcome: function (firstName) {
+      return `Hooray! Welcome Aboard, ${firstName}!!`
+   },
+   loginWelcome: function (firstName) {
+      return `Welcome ${firstName}!`
+   },
+   profileUpdated: 'User data successfully updated',
+   profileDeleted: 'User data successfully deleted',
+   alreadyInUse: 'Email already in use',
+   invalid: 'Oops! Invalid Credentials',
+   missingFields: 'You are missing some required fields!',
+   noUserData: 'Please supply user data!',
+   tokenInvalid: 'Token validation failed!',
+   supplyToken: 'Please supply token!',
+   newEntry: 'New entry successfully created!',
+   entryRemoved: function (role) {
+      return `${role} has been successfully removed from your list`
+   }
+}
